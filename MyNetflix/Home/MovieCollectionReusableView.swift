e664655e9170100e913fe4801dac24730ff3c5ad
dbguswls030 <dbguswls030@gmail.com>
2022-06-28 17:17:28 +0900

    feat : Home 탭 collectionView header, cell 추가하기

---
+++ MyNetflix/Home/MovieCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  MoiveCollectionReusableView.swift
+//  MyNetflix
+//
+//  Created by 유현진 on 2022/06/28.
+//
+
+import UIKit
+
+class MovieCollectionReusableView: UICollectionReusableView {
+        
+}
---
+++ MyNetflix/Home/MovieCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieCollectionViewCell.swift
+//  MyNetflix
+//
+//  Created by 유현진 on 2022/06/28.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+}
